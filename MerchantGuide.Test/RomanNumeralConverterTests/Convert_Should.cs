cb0f329f2c44fb030ebda19f2cb72235565cee26
Phil <philip.meier06@googlemail.com>
2016-03-06 16:22:04 +0000

    Addition of method for conversion of valid roman numerals

---
+++ MerchantGuide.Test/RomanNumeralConverterTests/Convert_Should.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MerchantGuide.Test.RomanNumeralConverterTests
+{
+    [TestClass]
+    public class Convert_Should
+    {
+        [TestMethod]
+        public void Translate_Numerals_Correctly()
+        {
+            Assert.AreEqual<int>(1, RomanNumeralConverter.Convert("I"));
+            Assert.AreEqual<int>(2, RomanNumeralConverter.Convert("II"));
+            Assert.AreEqual<int>(4, RomanNumeralConverter.Convert("IV"));
+            Assert.AreEqual<int>(5, RomanNumeralConverter.Convert("V"));
+            Assert.AreEqual<int>(10, RomanNumeralConverter.Convert("X"));
+            Assert.AreEqual<int>(2006, RomanNumeralConverter.Convert("MMVI"));
+            Assert.AreEqual<int>(1944, RomanNumeralConverter.Convert("MCMXLIV"));
+        }
+    }
+}
---
+++ MerchantGuide/RomanNumeralConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MerchantGuide
+{
+    public static class RomanNumeralConverter
+    {
+        private static readonly Dictionary<char, int> numeralConversion = new Dictionary<char, int>()
+        {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+
+        public static int Convert(string romanNumeral)
+        {
+           
+            int result = 0;
+            int last = 0;
+            foreach (char numeral in romanNumeral.Reverse())
+            {
+                var value = numeralConversion[numeral];
+                var addition = (value < last) ? -value : value;
+                result += addition;
+                last = value;
+            }
+            return result;
+        }
+    }
+}
